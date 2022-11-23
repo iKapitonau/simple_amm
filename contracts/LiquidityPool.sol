089df3871c8eb3a94d0bc67ed5d908ca1bb15cd9
Ilya Kapitonau <ilya.th.kapitonov@gmail.com>
2022-11-23 16:38:11 +0300

    Formatting

--- contracts/LiquidityPool.sol
+++ contracts/LiquidityPool.sol
@@ -11,69 +11,101 @@ contract LiquidityPool {
     address public immutable token2;
     uint public balance1;
     uint public balance2;
-    uint public total_shares;
-    mapping(address => uint256) shares;
+    uint public totalShares;
+    mapping(address => uint256) private shares;
 
     constructor(address _token1, address _token2) {
-        require(_token1 != address(0) && _token2 != address(0) && _token1 != _token2);
+        require(
+            _token1 != address(0) &&
+                _token2 != address(0) &&
+                _token1 != _token2,
+            "Invalid tokens"
+        );
         token1 = _token1;
         token2 = _token2;
     }
 
-    function add_liquidity(uint256 _amount1, uint256 _amount2) external {
-        require(_amount1 > 0 && _amount2 > 0);
+    function addLiquidity(uint256 _amount1, uint256 _amount2) external {
+        require(_amount1 > 0 && _amount2 > 0, "Amounts must be positive");
         if (balance1 > 0 && balance2 > 0)
-            require(balance1 * _amount2 == balance2 * _amount1, "x / y == dx / dy is not satisfied.");
+            require(
+                balance1 * _amount2 == balance2 * _amount1,
+                "x / y != dx / dy"
+            );
 
-        SafeERC20.safeTransferFrom(IERC20(token1), msg.sender, address(this), _amount1); 
-        SafeERC20.safeTransferFrom(IERC20(token2), msg.sender, address(this), _amount2); 
+        SafeERC20.safeTransferFrom(
+            IERC20(token1),
+            msg.sender,
+            address(this),
+            _amount1
+        );
+        SafeERC20.safeTransferFrom(
+            IERC20(token2),
+            msg.sender,
+            address(this),
+            _amount2
+        );
 
-        uint share = total_shares == 0 ?
-            Math.sqrt(_amount1 * _amount2) :
-            Math.min(
-                total_shares * _amount1 / balance1,
-                total_shares * _amount2 / balance2);
+        uint share = totalShares == 0
+            ? Math.sqrt(_amount1 * _amount2)
+            : Math.min(
+                (totalShares * _amount1) / balance1,
+                (totalShares * _amount2) / balance2
+            );
 
         balance1 += _amount1;
         balance2 += _amount2;
-        total_shares += share;
+        totalShares += share;
         shares[msg.sender] += share;
     }
 
-    function remove_liquidity(uint256 _share) external {
-        require(_share > 0 && _share <= shares[msg.sender]);
+    function removeLiquidity(uint256 _share) external {
+        require(_share > 0 && _share <= shares[msg.sender], "Wrong share");
 
-        uint256 amount1 = _share * balance1 / total_shares;
-        uint256 amount2 = _share * balance2 / total_shares;
+        uint256 amount1 = (_share * balance1) / totalShares;
+        uint256 amount2 = (_share * balance2) / totalShares;
         shares[msg.sender] -= _share;
-        total_shares -= _share;
+        totalShares -= _share;
         balance1 -= amount1;
         balance2 -= amount2;
 
-        SafeERC20.safeTransfer(IERC20(token1), msg.sender, amount1); 
-        SafeERC20.safeTransfer(IERC20(token2), msg.sender, amount2); 
+        SafeERC20.safeTransfer(IERC20(token1), msg.sender, amount1);
+        SafeERC20.safeTransfer(IERC20(token2), msg.sender, amount2);
     }
 
     function swap(address _token, uint256 _amount) external {
         require(_amount > 0, "Amount must be positive.");
-        require(_token == token1 || _token == token2, "No such token in the pool.");
-        require(IERC20(_token).allowance(msg.sender, address(this)) >= _amount, "Not approved tokens.");
+        require(
+            _token == token1 || _token == token2,
+            "No such token in the pool."
+        );
+        require(
+            IERC20(_token).allowance(msg.sender, address(this)) >= _amount,
+            "Not approved tokens."
+        );
 
         // fee == 0.3%
-        uint256 amount_after_fee = _amount * 997 / 1000;
+        uint256 amountAfterFee = (_amount * 997) / 1000;
 
-        SafeERC20.safeTransferFrom(IERC20(_token), msg.sender, address(this), _amount);
+        SafeERC20.safeTransferFrom(
+            IERC20(_token),
+            msg.sender,
+            address(this),
+            _amount
+        );
 
         if (_token == token1) {
-            uint256 amount_to_send = balance2 * amount_after_fee / (balance1 + amount_after_fee);
+            uint256 amountToSend = (balance2 * amountAfterFee) /
+                (balance1 + amountAfterFee);
             balance1 += _amount;
-            balance2 -= amount_to_send;
-            SafeERC20.safeTransfer(IERC20(token2), msg.sender, amount_to_send);
+            balance2 -= amountToSend;
+            SafeERC20.safeTransfer(IERC20(token2), msg.sender, amountToSend);
         } else {
-            uint256 amount_to_send = balance1 * amount_after_fee / (balance2 + amount_after_fee);
+            uint256 amountToSend = (balance1 * amountAfterFee) /
+                (balance2 + amountAfterFee);
             balance2 += _amount;
-            balance1 -= amount_to_send;
-            SafeERC20.safeTransfer(IERC20(token1), msg.sender, amount_to_send);
+            balance1 -= amountToSend;
+            SafeERC20.safeTransfer(IERC20(token1), msg.sender, amountToSend);
         }
     }
 }
--- contracts/LiquidityPoolFactory.sol
+++ contracts/LiquidityPoolFactory.sol
@@ -7,16 +7,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./LiquidityPool.sol";
 
 contract LiquidityPoolFactory is Ownable {
-    mapping(address => mapping(address => address)) public liquidity_pools;
+    mapping(address => mapping(address => address)) public liquidityPools;
 
-    function create_pool(address _token1, address _token2) public onlyOwner {
+    function createPool(address _token1, address _token2) public onlyOwner {
         // make tokens ordered
         if (_token1 > _token2) {
             (_token1, _token2) = (_token2, _token1);
         }
-        require(liquidity_pools[_token1][_token2] != address(0), "The pool already exists.");
-        require(_token1 != _token2, "Equal tokens are not allowed here.");
-        LiquidityPool liquidity_pool = new LiquidityPool(_token1, _token2);
-        liquidity_pools[_token1][_token2] = address(liquidity_pool);
+        require(
+            liquidityPools[_token1][_token2] != address(0),
+            "The pool already exists."
+        );
+        require(_token1 != _token2, "Equal tokens are not allowed.");
+        LiquidityPool liquidityPool = new LiquidityPool(_token1, _token2);
+        liquidityPools[_token1][_token2] = address(liquidityPool);
     }
 }
